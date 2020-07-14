ae54d5f29106eaf54026d3409e4923beeb8d8cb3
Woomymy <github@woomy.ovh>
2020-07-14 13:15:36 +0200

    Nonroot start

--- nonroot.sh
+++ nonroot.sh
@@ -1,1 +1,3 @@
 echo "Vérification du la capacité du système à être mis à jour..."
+yes | mintupgrade check
+yes | mintupgrade download
--- update.sh
+++ update.sh
@@ -3,6 +3,11 @@ if [ $UID == 0 ]; then
 	echo "Ce script ne doit pas être lancé en tant que root"
 	exit 1 
 fi
+echo "Afin d'installer la mise à jour, un accès root est requis. Entez votre mot de passe afin de lancer le script."
+sudo echo "Accès recu!"
+if [ $? == 1 ]; then
+        exit 1
+fi
 SCRIPTDIR="$HOME/woomy-update-mint"
 echo "Téléchargement des scripts..."
 ping -c 1 kernel.org > /dev/null
@@ -13,7 +18,7 @@ fi
 echo "Connecté au réseau!"
 if [ -d "$HOME/woomy-update-mint" ]; then
 	echo "Suppresion du dossier $SCRIPTDIR"
-	rm -rf "$HOME/woomy-update-mint"
+	sudo rm -rf "$HOME/woomy-update-mint"
 fi
 mkdir $SCRIPTDIR
 cd $SCRIPTDIR
@@ -21,10 +26,5 @@ echo "Download ${SCRIPTDIR}/root.sh script"
 curl https://raw.githubusercontent.com/Woomy4680-exe/mint-upgrade/master/root.sh > $SCRIPTDIR/root.sh
 echo "Download ${SCRIPTDIR}/nonroot.sh script"
 curl https://raw.githubusercontent.com/Woomy4680-exe/mint-upgrade/master/nonroot.sh > $SCRIPTDIR/nonroot.sh
-echo "Afin d'installer la mise à jour, un accès root est requis. Entez votre mot de passe afin de lancer le script."
-sudo echo "Accès recu!"
-if [ $? == 1 ]; then 
-	exit 1
-fi
 sudo bash "${SCRIPTDIR}/root.sh"
-
+bash "${SCRIPTDIR}/nonroot.sh"

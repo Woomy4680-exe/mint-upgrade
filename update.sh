9ee9c403e843e10400b7abff5c4341803fb4707d
Woomy4680-exe <matzar1808@gmail.com>
2020-07-14 12:52:45 +0200

    Curl

--- update.sh
+++ update.sh
@@ -1,8 +1,18 @@
-echo "Vérification des scripts..."
+#! /bin/bash
+SCRIPTDIR="$HOME/woomy-update-mint"
+echo "Téléchargement des scripts..."
 ping -c 1 kernel.org > /dev/null
 if [ $? != 0 ]; then 
 	echo "Pas de connection réseau!"
 	exit 1
 fi 
 echo "Connecté au réseau!"
+if [ -d "$HOME/woomy-update-mint" ]; then
+	echo "Suppresion du dossier $SCRIPTDIR"
+	rm -rf "$HOME/woomy-update-mint"
+fi
+mkdir $SCRIPTDIR
+cd $SCRIPTDIR
+echo "Download ${SCRIPTDIR}/root.sh script"
+curl https://raw.githubusercontent.com/Woomy4680-exe/mint-upgrade/master/root.sh > $SCRIPTDIR/root.sh
 
